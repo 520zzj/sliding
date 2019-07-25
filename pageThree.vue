d2ec7244d6a9e7148d4a42b3408091be3ffc46b8
520zzj <44475605+520zzj@users.noreply.github.com>
2019-07-26 03:08:48 +0800

    Create pageThree.vue

---
+++ pageThree.vue
@@ -0,0 +1,31 @@
+<template>
+    <div class="pageThree" :style="{width:this.$store.state.width_s+'px'}">
+           <v-touch @swiperight="menuTwo">
+                <div class="page">页面3</div>
+           </v-touch>
+
+    </div>
+</template>
+<script>
+export default {
+    data(){
+        return{
+
+        }
+    },
+    methods:{
+        menuTwo(){
+            this.$router.push({"path":"/pageTwo"})
+        }
+    }
+}
+</script>
+<style>
+.pageThree .page{
+    width:100%;
+    height: 400px;
+    background:green;
+}
+</style>
+
+
